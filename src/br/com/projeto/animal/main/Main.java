306fe1a93b6019c413fa20ee20fdd9e9c5409c90
Fabricio Nathan <fabricionathan749@gmail.com>
2025-03-23 14:15:13 -0300

    projeto pronto

--- src/br/com/projeto/animal/main/Main.java
+++ src/br/com/projeto/animal/main/Main.java
@@ -1,4 +1,30 @@
 package br.com.projeto.animal.main;
 
+import br.com.projeto.animal.model.Animal;
+
 public class Main {
+    public static void main(String[]args){
+        Animal animal1=new Animal("Ave","Papagaio","verde",60);
+        Animal animal2=new Animal("Mamifero","Elefante","Cinza",70);
+
+        System.out.println("**********************************************************************");
+        System.out.println("           EXIBINDO INFORMACOES DO PRIMEIRO ANIMAL");
+        System.out.println("**********************************************************************");
+        System.out.println();
+        animal1.exibirInfor();
+        System.out.println();
+        animal1.calcularIdadeEmMeses();
+        System.out.println();
+        System.out.println("**********************************************************************");
+        System.out.println("           EXIBINDO INFORMACOES DO SEGUNDO ANIMAL");
+        System.out.println("**********************************************************************");
+        System.out.println();
+        animal2.exibirInfor();
+        System.out.println();
+        animal2.calcularIdadeEmMeses();
+
+
+
+
+    }
 }
--- src/br/com/projeto/animal/model/Animal.java
+++ src/br/com/projeto/animal/model/Animal.java
@@ -1,4 +1,21 @@
 package br.com.projeto.animal.model;
 
 public class Animal {
+    public String tipo;
+    public String nome;
+    public String cor;
+    public int qtddeanosquesobrevive;
+
+    public Animal(String tipo,String nome,String cor,int qtddeanosquesobrevive){
+        this.tipo=tipo;
+        this.nome=nome;
+        this.cor=cor;
+        this.qtddeanosquesobrevive=qtddeanosquesobrevive;
+    }
+    public void exibirInfor(){
+        System.out.printf("O tipo do animal é %s, o nome dele é %s, a cor dele é %s, e ele sobrevive por %d anos",this.tipo,this.nome,this.cor,this.qtddeanosquesobrevive);
+    }
+    public void calcularIdadeEmMeses(){
+        System.out.printf("A quantidade de meses do animal é %d",this.qtddeanosquesobrevive*12);
+    }
 }
